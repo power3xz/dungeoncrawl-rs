0f6332263eb0614fdf634b49eb6ae12468446426
devj <power3xz@gmail.com>
2022-06-20 23:56:43 +0900

    map 구조체 render 메서드 추가

--- src/map.rs
+++ src/map.rs
@@ -7,6 +7,10 @@ pub enum TileType {
     Floor,
 }
 
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
 pub struct Map {
     pub tiles: Vec<TileType>,
 }
@@ -15,6 +19,22 @@ impl Map {
     pub fn new() -> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    }
+                    TileType::Wall => {
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+                    }
+                }
+            }
         }
     }
 }
