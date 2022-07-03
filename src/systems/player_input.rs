d6146e17c82b5359475d8a3b0e81665bd788be3f
devj <power3xz@gmail.com>
2022-07-03 22:27:38 +0900

    회복 조건 수정

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -21,14 +21,14 @@ pub fn player_input(
         };
 
         let mut did_something = false;
+        let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
-
-            let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
-            let (player_entity, destination) = players
-                .iter(ecs)
-                .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
-                .unwrap();
+            did_something = true;
 
             let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
             enemies
@@ -36,7 +36,6 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
-                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -55,16 +54,17 @@ pub fn player_input(
                     },
                 ));
             }
-            if !did_something {
-                if let Ok(mut health) = ecs
-                    .entry_mut(player_entity)
-                    .unwrap()
-                    .get_component_mut::<Health>()
-                {
-                    health.current = i32::min(health.max, health.current + 1);
-                }
+        }
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
+
         *turn_state = TurnState::PlayerTurn;
     }
 }
