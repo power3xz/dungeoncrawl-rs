46b8c6438ba2792706c8a2cfb218f7a8087ece1e
devj <power3xz@gmail.com>
2022-07-03 22:21:10 +0900

    player 회복 추가

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -18,6 +20,7 @@ pub fn player_input(
             _ => Point::new(0, 0),
         };
 
+        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
 
@@ -33,6 +36,7 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -50,6 +54,15 @@ pub fn player_input(
                         destination,
                     },
                 ));
+            }
+            if !did_something {
+                if let Ok(mut health) = ecs
+                    .entry_mut(player_entity)
+                    .unwrap()
+                    .get_component_mut::<Health>()
+                {
+                    health.current = i32::min(health.max, health.current + 1);
+                }
             }
         }
         *turn_state = TurnState::PlayerTurn;
